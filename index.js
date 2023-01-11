2f280c8bdad0010048e88878521703319320f2eb
AdaN6 <a_disney_da@hotmail.com>
2023-01-11 16:19:46 +0100

    added urlencoded middleware

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const express = require ('express');
-const app = express();
 require("dotenv").config();
 
+const express = require("express");
+const app = express();
 const cors = require("cors");
-app.use(cors());
+
+const middlewares = [
+    express.urlencoded({extended:false}),
+    express.json(), 
+    cors()];
+app.use(middlewares);
 
 const port = process.env.PORT || 6000;
 
 // --> connect to mongo
 const connectToDB = require("./DB/mongoConnection");
 
-
-const newsRoutes = require("./routes/newsRoute")
+const newsRoutes = require("./routes/newsRoute");
 app.use("/news", newsRoutes);
 
 connectToDB().then(() => {
