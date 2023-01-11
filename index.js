a2dc11378471c635ebf664e1eb9b3e2e3275445f
AdaN6 <a_disney_da@hotmail.com>
2023-01-11 12:17:29 +0100

    added mongoose.set strictQuery to false

--- index.js
+++ index.js
@@ -1,32 +1,19 @@
 const express = require ('express');
 const app = express();
 require("dotenv").config();
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
-};
 
 const cors = require("cors");
 app.use(cors());
 
-
 const port = process.env.PORT || 6000;
 
 // --> connect to mongo
-// const connectToDB = require("./DB/mongoConnection");
-
+const connectToDB = require("./DB/mongoConnection");
 
 
 const newsRoutes = require("./routes/newsRoute")
 app.use("/news", newsRoutes);
 
-connectDB().then(() => {
+connectToDB().then(() => {
   app.listen(port, () => console.log(`Server started on port ${port}`));
 });
