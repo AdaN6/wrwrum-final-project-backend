3ee4e991ee1c2d3108f57e8b124e858b95d7e0be
AdaN6 <a_disney_da@hotmail.com>
2023-01-11 12:01:40 +0100

    changed uri to OpenUri

--- DB/mongoConnection.js
+++ DB/mongoConnection.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const connectToDB = async () => {
-    await mongoose.connect(process.env.MONGO_URI), () => console.log('Connected to MongoDB')
+    await mongoose.connect(process.env.MONGO_OPENURI),
+      () => console.log("Connected to MongoDB");
 }
 
 module.exports = connectToDB
