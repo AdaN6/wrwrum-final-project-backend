70eed2fe01abf489100d572dd73d10b1c3cd313f
AdaN6 <a_disney_da@hotmail.com>
2023-01-11 17:58:21 +0100

    reconnect mongo

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-require("dotenv").config();
-
 const express = require("express");
 const app = express();
 const cors = require("cors");
 
+require("dotenv").config();
+
 const middlewares = [
-    express.urlencoded({extended:false}),
-    express.json(), 
-    cors()];
+  express.urlencoded({ extended: false }),
+  express.json(),
+  cors(),
+];
 app.use(middlewares);
 
 const port = process.env.PORT || 5000;
 
 // --> connect to mongo
-// const connectToDB = require("./DB/mongoConnection");
+const connectToDB = require("./DB/mongoConnection");
+connectToDB()
 
 const newsRoutes = require("./routes/newsRoute");
 app.use("/news", newsRoutes);
 
 
-  app.listen(port, () => console.log(`Server started on port ${port}`));
+
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
 
