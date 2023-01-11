91807763947d5b1764ebd2d270086b905424674e
AdaN6 <a_disney_da@hotmail.com>
2023-01-11 12:29:43 +0100

    changed the uri to string

--- DB/mongoConnection.js
+++ DB/mongoConnection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.set("strictQuery", true);
+mongoose.set("strictQuery", false);
 const connectToDB = async () => {
     await mongoose.connect(process.env.MONGO_URI),
       () => console.log("Connected to MongoDB");
