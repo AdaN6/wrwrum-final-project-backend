a9e9bef621f7c721a681bc280880f63531d93f33
AdaN6 <a_disney_da@hotmail.com>
2023-01-11 12:04:47 +0100

    test 3 mongo testing with cyclic

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require ('express');
 const app = express();
 require("dotenv").config();
+const mongoose = require("mongoose");
 
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
 const cors = require("cors");
 app.use(cors());
@@ -10,13 +20,13 @@ app.use(cors());
 const port = process.env.PORT || 6000;
 
 // --> connect to mongo
-const connectToDB = require("./DB/mongoConnection");
+// const connectToDB = require("./DB/mongoConnection");
 
 
 
 const newsRoutes = require("./routes/newsRoute")
 app.use("/news", newsRoutes);
 
-connectToDB().then(() => {
- app.listen(port, () => console.log(`Server started on port ${port}`));
-})
+connectDB().then(() => {
+  app.listen(port, () => console.log(`Server started on port ${port}`));
+});
