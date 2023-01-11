eff70837737955a161ed419003a14d054063604c
AdaN6 <a_disney_da@hotmail.com>
2023-01-11 12:19:35 +0100

    strictQuery changed to true

--- DB/mongoConnection.js
+++ DB/mongoConnection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.set("strictQuery", false);
+mongoose.set("strictQuery", true);
 const connectToDB = async () => {
     await mongoose.connect(process.env.MONGO_URI),
       () => console.log("Connected to MongoDB");
