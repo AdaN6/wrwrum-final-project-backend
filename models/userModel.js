cbbc5d7144c3d26fa78b2f6e578807c45aa4d6c2
AdaN6 <a_disney_da@hotmail.com>
2023-01-12 11:54:46 +0100

    created a userSchema

---
+++ models/userModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema ({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    country: {
+        type: String
+    },
+    favourtieDriver: {
+        type: String
+    },
+    favouriteTeam: {
+        type: String
+    },
+    image: {
+        type: String
+    },
+    status: {
+        type: Boolean
+    }
+
+})
+
+module.exports = mongoose.model("User", userSchema)
