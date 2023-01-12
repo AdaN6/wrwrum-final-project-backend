afd79548f5dea998a1c0de5ade15fa9e4c9c7d75
AdaN6 <a_disney_da@hotmail.com>
2023-01-12 16:58:46 +0100

    salt and has pasword with bcrypt for signup user

--- models/userModel.js
+++ models/userModel.js
@@ -45,6 +45,12 @@ userSchema.static.signup = async (email, password) => {
     if (exist) {
         throw Error('Email already in use')
     }
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    const user = await this.create({email, password: hash});
+
+    return user;
 }
 
 module.exports = mongoose.model("User", userSchema)
