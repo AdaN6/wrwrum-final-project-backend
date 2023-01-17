8939e926c805fe3f40fb61a38eca500a39b5bf9e
Wiktor Szymczyk <wiktor.szymczyk221@gmail.com>
2023-01-17 14:45:07 +0100

    temp

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const http = require('http');
 const httpServer = http.createServer(app);
 const { Server } = require("socket.io");
 const cors = require("cors");
-const newsRoutes = require("./routes/newsRoute")
-
 const userRoutes = require("./routes/usersRouter");
 
 const port = process.env.PORT || 6000;
@@ -40,7 +38,7 @@ app.use((req,res,next)=>{
 app.use(cors());
 const io = new Server(httpServer, {
   cors: {
-    origin: process.env.REACT_APP_FRONTEND
+    origin: 'http://localhost:3000'
   }
 });
 app.use("/news", newsRoutes);
@@ -64,4 +62,5 @@ io.on("connection", (socket) => {
 httpServer.listen(7000, () => {
   console.log(`listening on port ${port}`);
 });
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
