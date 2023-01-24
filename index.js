784192ee5f4cad226304dfbd600855e71774bf5d
AdaN6 <a_disney_da@hotmail.com>
2023-01-24 15:58:49 +0100

    comment out socket.io lines

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 require("dotenv").config();
 const express = require('express');
 const app = express();
-const http = require('http');
-const httpServer = http.createServer(app);
-const { Server } = require("socket.io");
 const cors = require("cors");
 
 const port = process.env.PORT || 7000;
@@ -18,7 +15,6 @@ app.use(middlewares);
 app.use((_req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
-  
   next();
 });
 
@@ -31,39 +27,38 @@ const userRoutes = require("./routes/usersRouter");
 app.use("/news", newsRoutes);
 app.use("/api/user", userRoutes);
 
-app.use((req,res,next)=>{
-  console.log(req.path,req.method)
-  next()
-})
-app.use(cors());
+// app.use((req,res,next)=>{
+//   console.log(req.path,req.method)
+//   next()
+// })
+// app.use(cors());
 
-const io = new Server(httpServer, {
-  cors: {
-    origin: '*',
-    methods: ["GET", "POST"],
-  }
-});
+// const io = new Server(httpServer, {
+//   cors: {
+//     origin: '*',
+//     methods: ["GET", "POST"],
+//   }
+// });
 
-app.use("/news", newsRoutes);
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
 });
 
-io.on("connection", (socket) => {
-  console.log("c", socket.id)
-  socket.on('send', (message, userName, timestamp, colorMe, colorOther) => {
-    socket.broadcast.emit('receive_message', {message, userName, timestamp, colorMe, colorOther})
-  });
+// io.on("connection", (socket) => {
+//   console.log("c", socket.id)
+//   socket.on('send', (message, userName, timestamp, colorMe, colorOther) => {
+//     socket.broadcast.emit('receive_message', {message, userName, timestamp, colorMe, colorOther})
+//   });
   
   
-  io.on('disconnect', (socket) => {
-    console.log('user disconnected', socket.id)
-  })
-});
+//   io.on('disconnect', (socket) => {
+//     console.log('user disconnected', socket.id)
+//   })
+// });
 
-httpServer.listen(port, () => {
-  console.log(`listening on port ${port}`);
-});
+// httpServer.listen(port, () => {
+//   console.log(`listening on port ${port}`);
+// });
 
-// app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => console.log(`Server started on port ${port}`));
